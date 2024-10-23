74235d4024b4144a2f9b4a1820ca774a270e1f31
Sean Arnoldt <sean.arnoldt6@gmail.com>
2024-10-23 15:07:56 -0500

    Mode methods to photoservce

--- src/main/java/com/seanarnoldt/photos/PhotoController.java
+++ src/main/java/com/seanarnoldt/photos/PhotoController.java
@@ -23,10 +23,11 @@ import jakarta.validation.Valid;
 @RestController
 public class PhotoController {
 
-    private Map<String, Photo> db = new HashMap<>(){{
-        put("1", new Photo("1", "hello.jpg"));
-    }};
+    private final PhotoService photoService;
 
+    public PhotoController(PhotoService photoService) {
+        this.photoService = photoService;
+    }
 
     @GetMapping("/")
     public String hello() {
@@ -35,29 +36,24 @@ public class PhotoController {
 
     @GetMapping("/photos")
     public Collection<Photo> get(){
-        return db.values();
+        return photoService.get();
     }
 
     @GetMapping("/photos/{id}")
     public Photo get(@PathVariable String id){
-        Photo photo = db.get(id);
+        Photo photo = photoService.get(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return photo;
     }
 
     @DeleteMapping("/photos/{id}")
     public void delete(@PathVariable String id){
-        Photo photo = db.remove(id);
+        Photo photo = photoService.remove(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/photos")
     public Photo create(@RequestPart("data") MultipartFile file) throws IOException{
-        Photo photo = new Photo();
-        photo.setId(UUID.randomUUID().toString());
-        photo.setFileName(file.getOriginalFilename());
-        photo.setData(file.getBytes());
-        db.put(photo.getId(), photo);
-        return photo;
+        return photoService.save(file.getOriginalFilename(), file.getContentType(), file.getBytes());
     }
 }

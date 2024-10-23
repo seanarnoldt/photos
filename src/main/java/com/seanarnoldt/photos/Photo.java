0f2938075ca33d31f4133d673bb38ff64fecbb51
Sean Arnoldt <sean.arnoldt6@gmail.com>
2024-10-23 13:28:55 -0500

    create photo class

---
+++ src/main/java/com/seanarnoldt/photos/Photo.java
@@ -0,0 +1,22 @@
+package com.seanarnoldt.photos;
+
+public class Photo {
+     private String id; 
+     private String fileName;
+
+     public String getId() {
+        return id;
+     }
+
+     public void setId(String id){
+        this.id = id;
+     }
+
+     public String getFileName(){
+        return fileName;
+     }
+
+     public void setFileName(String fileName){
+        this.fileName = fileName;
+     }
+}
